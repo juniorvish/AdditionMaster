4a5766f5aa7d33357475991c805dbc2f57ccf3d7
builderbot <135554020+juniorvish@users.noreply.github.com>
2023-10-22 20:10:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-a = 5
-b = 6
-print(a+b)
+# Get user input for the two numbers to be added
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Add the two numbers
+sum = num1 + num2
+
+# Print the result
+print(f"The sum of {num1} and {num2} is {sum}")
